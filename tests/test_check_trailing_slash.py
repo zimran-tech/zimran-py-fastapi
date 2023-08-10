50a715c90cbf027753f2c76a109b2504213df4e7
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-08-11 01:45:06 +0600

    add better check_trailing_slash test

--- tests/test_check_trailing_slash.py
+++ tests/test_check_trailing_slash.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 
+@pytest.mark.xfail(raises=AssertionError, strict=True)
 async def test(app: FastAPI) -> None:
-    with pytest.raises(AssertionError):
-        with TestClient(app):
-            pass
+    with TestClient(app):
+        pass
