e7352b38cda4ba5aa1d47a3790536f3dd0f44f14
Bauyrzhan <bauyzhan.balgaziyev@zimran.io>
2024-01-29 13:29:32 +0600

    fix: lifespan fixed

--- zimran/fastapi/__init__.py
+++ zimran/fastapi/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 from contextlib import asynccontextmanager
 from typing import Any
 
@@ -35,21 +36,29 @@ def create_app(environment: Environment, **kwargs) -> FastAPI:  # type: ignore
     kwargs.setdefault('title', 'Zimran App')
     kwargs.update(_get_application_docs_kwargs(environment))
 
-    @asynccontextmanager  # type: ignore
-    async def _lifespan(app: FastAPI) -> None:
-        for route in app.routes:
-            assert route.path.endswith('/'), f"Route '{route.path}' must end with '/'"  # type: ignore # noqa: E501
+    async def _check_trailing_slash(routes):
+        for route in routes:
+            assert route.path.endswith('/'), f"Route '{route.path}' must end with '/'"
 
+    async def _check_deprecated_hooks(app: FastAPI):
         if any([
             app.router.on_startup, app.router.on_shutdown,
             kwargs.get('on_startup'), kwargs.get('on_shutdown'),
         ]):
             raise Exception('Cannot use on_startup or on_shutdown with lifespan context manager')
 
-        if lifespan := kwargs.pop('lifespan', None):
-            async with lifespan(app):
+    if lifespan_ := kwargs.pop('lifespan', None):
+        @asynccontextmanager
+        async def _lifespan(app: FastAPI) -> None:
+            async with lifespan_(app):
+                await asyncio.gather(_check_trailing_slash(app.routes), _check_deprecated_hooks(app))
+
                 yield
-        else:
+    else:
+        @asynccontextmanager
+        async def _lifespan(app: FastAPI) -> None:
+            await asyncio.gather(_check_trailing_slash(app.routes), _check_deprecated_hooks(app))
+
             yield
 
     kwargs['lifespan'] = _lifespan
